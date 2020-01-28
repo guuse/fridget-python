97f3266862c1508602ca854c17f8c8fbfce7a739
Aaron <kyrumx@gmail.com>
2020-01-28 10:42:27 +0100

    Cleaning + fixing the scan thread

--- ScanLoopThread.py
+++ ScanLoopThread.py
@@ -14,11 +14,15 @@ except (RuntimeError, ModuleNotFoundError):
 class ScanLoopThread(QThread):
     """Class which runs our scan loop.
 
-    This needs to run in a different thread.
+    This runs in a different thread.
     It starts looking for a signal from our IR sensor, when found it will send a signal
     to the scanner to activate.
     Once it has scanned a barcode it emit a signal to talk with a different thread (the UI thread)
     so that the newly scanned item can be added to the ListWidget.
+
+    self.ean is updated every time by the main thread (when the main thread notices that the hidden
+    input field has changed).
+
     """
     scanned_signal = pyqtSignal(str)
     clear_label_signal = pyqtSignal()
@@ -27,36 +31,44 @@ class ScanLoopThread(QThread):
     def __init__(self):
         QThread.__init__(self)
         self.ean = ""
+        self.scanning = True
 
     def __del__(self):
         self.wait()
 
     def _loop(self):
-        time.sleep(1.5)
-        self.scanned_ean = ""
-        self.scanning = True
+        """Scan loop function
+
+        This function runs a loop, ensures that the barcode scanner is off by sending a singal to the scanner pin.
+        The inner loop is active as long as the IR pin is active (e.g. sees something).
+        We must set focus on the hidden input field (the scanner literally inserts its scanned value into an active
+        field, so we must ensure the hidden field is active).
+        We take the self.ean value and put it into a local variable (since it may be updated by the main thread while
+        we are still using it, we need to store it locally).
+        If the found ean has a length of 13 we stop the loop and return the found ean.
+        """
+        time.sleep(0.5)
+        scanned_ean = ""
         while self.scanning:
-            print("SCANNING")
             self.clear_label_signal.emit()
             RPi.GPIO.output(settings.SCANNER_PIN, RPi.GPIO.HIGH)
             while not RPi.GPIO.input(settings.IR_PIN) and self.scanning:
                 self.set_focus_signal.emit()
-                print("!!!")
                 RPi.GPIO.output(settings.SCANNER_PIN, RPi.GPIO.HIGH)
                 RPi.GPIO.output(settings.SCANNER_PIN, RPi.GPIO.LOW)
-                self.scanned_ean = self.ean
-                print(self.scanned_ean)
+                scanned_ean = self.ean
                 if len(self.scanned_ean) == 13:
-                    print("EAN FOUND")
-                    print(self.scanned_ean)
+                    print("EAN FOUND: " + self.scanned_ean)
                     RPi.GPIO.output(settings.SCANNER_PIN, RPi.GPIO.HIGH)
                     self.scanning = False
-
-        return self.scanned_ean
+                    return scanned_ean
 
     def run(self):
+        """Run method of the thread.
+
+        Starts the _loop() and waits for a valid ean to be found, if found it emits a signal to the main thread
+        which will add it to the ui.
+        """
         ean = self._loop()
-        print("DEZE EAN")
-        print(ean)
         self.scanned_signal.emit(ean)
 
